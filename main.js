3e278ce34aa0d9823fe82f4e87afce3a3c936881
SabahudinMusinovic <105946462+SabahudinMusinovic@users.noreply.github.com>
2022-05-21 20:29:28 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -48,17 +48,116 @@ document.querySelector(".sumbit-button").addEventListener("click", () => {
   addFoodListItem();
 });
 const deleteFoodListItem = (id) => {
-  fetch("https://ptf-web-dizajn-2022.azurewebsites.net/api/Food", {
+  fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food/0`, {
     method: "DELETE",
   }).then((res) => {
     console.log(res);
   });
 };
-const completeFoodListItem = (id) => {
+const completeFoodListItem = () => {
+  const foodid = document.querySelector(".food-id").value;
+  const foodname = document.querySelector(".food-name").value;
+  const foodprice = document.querySelector(".food-price").value;
+  const foodimageUrl = document.querySelector(".food-imageUrl").value;
   fetch("https://ptf-web-dizajn-2022.azurewebsites.net/api/Food", {
     method: "PUT",
+    headers: { "Content-type": "application/json;charset=UTF-8" },
+    body: JSON.stringify({
+      id: Number(foodid),
+      name: String(foodname),
+      price: Number(foodprice),
+      imageUrl: String(foodimageUrl),
+    })
   }).then((res) => {
     console.log(res);
+  }); 
+};
+
+document.querySelector(".dodaj-button").addEventListener("click", () => {
+  document.querySelector(".dodaj").style.display = "block";
+  document.querySelector(".update").style.display = "none";
+  document.querySelector(".izbrisi").style.display = "none";
+});
+document.querySelector(".izmjena-button").addEventListener("click", () => {
+  document.querySelector(".update").style.display = "block";
+  document.querySelector(".dodaj").style.display = "none";
+  document.querySelector(".izbrisi").style.display = "none";
+});
+document.querySelector(".brisi-button").addEventListener("click", () => {
+  document.querySelector(".izbrisi").style.display = "block";
+  document.querySelector(".dodaj").style.display = "none";
+  document.querySelector(".update").style.display = "none";
+});
+
+
+const menuBar = document.querySelector("#menu-bar");
+const navbar = document.querySelector(".navbar");
+
+
+const catagory = document.querySelectorAll(".catagory input");
+const cataImg = document.querySelector("#c-img");
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar ul li a");
+
+menuBar.addEventListener("click", () => {
+  menuBar.classList.toggle("fa-times");
+  menuBar.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
+
+
+document.addEventListener("scroll", () => {
+  menuBar.classList.remove("fa-times");
+  menuBar.classList.remove("active");
+  navbar.classList.remove("active");
+
+
+  connectSecWithNavLink();
+});
+
+catagory.forEach((element) => {
+  element.addEventListener("click", () => {
+    catagory.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+
+    let values = element.value;
+    element.classList.add("active");
+    
+  });
+});
+
+
+const connectSecWithNavLink = () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    let linkAttribute = link.attributes.href.value;
+    link.classList.remove("active");
+
+    if (linkAttribute === `#${current}`) {
+      link.classList.add("active");
+    }
   });
 };
 
+let deleteId=document.querySelector('.food-id')
+let deleteForm=document.querySelector('.deleteForm')
+let updateForma=document.querySelector('.updateForma')
+deleteForm.addEventListener("submit",(e)=>{
+  e.preventDefault() 
+  deleteFoodListItem(Number(deleteId.value))
+})  
+updateForma.addEventListener("submit",(e)=>{
+  e.preventDefault() 
+  completeFoodListItem()
+})  
